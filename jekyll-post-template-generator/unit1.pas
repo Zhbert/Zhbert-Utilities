1a72434952ddbb959396a90b3251905066c33171
Zhbert <zhbert@yandex.ru>
2021-12-22 21:51:39 +0300

    Main form

--- jekyll-post-template-generator/unit1.pas
+++ jekyll-post-template-generator/unit1.pas
@@ -5,10 +5,30 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Calendar,
+  Buttons;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    BitBtn1: TBitBtn;
+    BitBtn2: TBitBtn;
+    Calendar1: TCalendar;
+    Calendar2: TCalendar;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Edit4: TEdit;
+    Edit5: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
   private
 
   public
