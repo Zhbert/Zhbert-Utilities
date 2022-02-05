008be383ecf565c7bcde94e0bd999c0e9b216485
Zhbert <zhbert@yandex.ru>
2022-02-05 22:20:59 +0300

    Replace spaces and convert to down case

--- downcaser/src/ru/zhbert/downcaser/Main.java
+++ downcaser/src/ru/zhbert/downcaser/Main.java
@@ -3,6 +3,8 @@ package ru.zhbert.downcaser;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        for (String arg : args) {
+            System.out.println(arg.toLowerCase().replaceAll("\\s+", "_"));
+        }
     }
 }
